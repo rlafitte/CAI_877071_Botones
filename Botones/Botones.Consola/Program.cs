360ee625f39e4bd291547261d8ab04d49cbbdddf
Renard <rdlafitte@gmail.com>
2021-04-03 14:24:47 -0300

    Inicio solución

---
+++ Botones/Botones.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botones.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
